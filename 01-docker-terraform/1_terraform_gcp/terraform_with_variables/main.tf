d08e713a61f72fe73752d2dd86868b52e02b8fa8
arjunrarjunr <arjun121094@gmail.com>
2025-02-17 10:49:19 +0530

    dlthub workshop assignment

--- 01-docker-terraform/1_terraform_gcp/terraform_with_variables/main.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform_with_variables/main.tf
@@ -43,59 +43,60 @@ resource "google_storage_bucket" "arjun_nyc_taxi_bucket" {
 }
 
 
-# resource "google_compute_instance" "arjun_compute_engine" {
-#     name         = var.gcs_vm_name
-#     project      = "de-zoomcamp-448516"
-#     zone         = "asia-south1-c"
-#     machine_type = "e2-standard-8"
+resource "google_compute_instance" "arjun_compute_engine" {
+    name         = var.gcs_vm_name
+    project      = "de-zoomcamp-448516"
+    zone         = "asia-south1-c"
+    machine_type = "e2-standard-8"
 
-#     boot_disk {
-#         initialize_params {
-#             image = "projects/ubuntu-os-cloud/global/images/ubuntu-2404-noble-amd64-v20250130"
-#             size  = 30
-#             type  = "pd-standard"  # Change this to "pd-standard" or "pd-ssd"
-#         }
-#         auto_delete = true
-#     }
+    boot_disk {
+        initialize_params {
+            image = "projects/ubuntu-os-cloud/global/images/ubuntu-2404-noble-amd64-v20250130"
+            size  = 30
+            type  = "pd-standard"  # Change this to "pd-standard" or "pd-ssd"
+        }
+        auto_delete = true
+    }
 
-#     network_interface {
-#         network       = "default"
-#         access_config {
-#             network_tier = "PREMIUM"
-#         }
-#     }
+    network_interface {
+        network       = "default"
+        access_config {
+            network_tier = "PREMIUM"
+        }
+    }
 
-#     service_account {
-#         email  = "terraform-runner@de-zoomcamp-448516.iam.gserviceaccount.com"
-#         scopes = ["cloud-platform"]
+    service_account {
+        email  = "terraform-runner@de-zoomcamp-448516.iam.gserviceaccount.com"
+        scopes = ["cloud-platform"]
 
-#     }
+    }
 
-#     scheduling {
-#         automatic_restart   = true
-#         on_host_maintenance = "MIGRATE"
-#         preemptible         = false
-#     }
+    scheduling {
+        automatic_restart   = true
+        on_host_maintenance = "MIGRATE"
+        preemptible         = false
+    }
 
-#     shielded_instance_config {
-#         enable_secure_boot          = false
-#         enable_vtpm                 = true
-#         enable_integrity_monitoring = true
-#     }
+    shielded_instance_config {
+        enable_secure_boot          = false
+        enable_vtpm                 = true
+        enable_integrity_monitoring = true
+    }
 
-#     labels = {
-#         goog-ec-src = "vm_add-gcloud"
-#     }
+    labels = {
+        goog-ec-src = "vm_add-gcloud"
+    }
 
-#     metadata_startup_script = file("gcp_vm_install_docker.sh")
+    metadata_startup_script = file("gcp_vm_install_docker.sh")
 
     
-# }
+}
 
 resource "google_bigquery_dataset" "nyc_taxi_dataset" {
     dataset_id = var.bq_dataset_name
     project = var.project
     location = var.region
+    delete_contents_on_destroy = true
     default_table_expiration_ms = 7200000
     default_partition_expiration_ms = 7200000
     labels = {
